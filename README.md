007400ab2632412add6073553af0350412e992ad
Anil Rayamajhi <anilrm07@gmail.com>
2017-02-22 11:44:41 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+aqua_note
+=========
+
+A Symfony project created on February 22, 2017, 6:44 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+// class AppBundle extends Bundle  [React]
+
+class GenusController
+{
+  //Route
+  /**
+  *@Route('/genus')
+  */
+
+
+
+}
